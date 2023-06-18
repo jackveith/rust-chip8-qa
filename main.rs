228dd75b3514a5c8eb0f7a6f7ef257f839f9f509
GxGahs <jvarsity2002@gmail.com>
2023-06-18 19:48:11 -0400

    added ch8 programs

--- main.rs
+++ main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::Read;
 use rand::random;
 use std::io;
-
+use tokio::time::{ sleep, Duration };
 
 const DISPLAY_WIDTH:  usize = 64;
 const DISPLAY_HEIGHT: usize = 32;
@@ -28,6 +28,7 @@ fn main() {
     read_program_into_memory(&mut memory); 
     load_font_into_memory(&mut memory);
 
+    let mut its = 0;
     loop {
 
         let current_instruction = [memory[pc], memory[pc+1]];
@@ -40,9 +41,6 @@ fn main() {
             current_instruction[1] &  0xF,
         ];
 
-        for a in current_instruction_nibbles {
-            print!["{}, ", a];
-        }   println![];
 
         match current_instruction_nibbles {
             [    0,    0,  0xE,    0] => clear_screen(&mut display),
@@ -68,12 +66,22 @@ fn main() {
 
                 
 
-            [0,0,0,0] => break,
+            [0,0,0,0] => (),
                 [_, _, _, _] => println!["failure to parse"],
 
         }
 
         print_display(&mut display);
+        for a in current_instruction_nibbles {
+            print!("{}, ", a);
+        }   println!();
+        for a in 0..16 {
+            print!("{}, ", g_register[a]);
+        }   println!();
+        //sleep(Duration::from_millis(5000));
+
+        its += 1;
+        if its >= 60 { break }
 
     }
 
@@ -83,45 +91,12 @@ fn main() {
     //
 }
 
-fn special_register_operation (g_register: &mut [u8], memory: &mut [u8], timer_delay: &mut u8, timer_sound: &mut u8, ir: &mut u16, regx: u8, b: u8, c: u8) {
-    let x = regx as usize;
-    match (b, c) {
 
-        //TIMER INTO REG
-        (0,   7) => g_register[x] = *timer_delay,
-        //TODO: KEYPRESS TO REG
-        (0, 0xA) => (),
-        //REG INTO DELAY
-        (1,   5) => *timer_delay = g_register[x],
-        //REG INTO SOUND
-        (1,   8) => *timer_sound = g_register[x],
-        //IR = IR + REG
-        (1, 0xE) => *ir = *ir + g_register[x] as u16,
-        //TODO: IMPLEMENT LOAD SPRITE ADDR TO IR
-        (2,   9) => (),
-        //BINARY CODED DECIMAL
-        (3,   3) => {
-            let digits = [g_register[x] / 100, g_register[x] /10 % 10, g_register[x] % 10];
-            for i in 0..3 { memory[*ir as usize + i as usize] = digits[i] }
-        }
-        //FILL REG INTO MEM
-        (5,   5) => {
-            for i in 0..(regx + 1) { memory[(*ir + i as u16) as usize] = g_register[i as usize] }
-            *ir = *ir + 1 + regx as u16;   
-        },
-        //FILL MEM INTO REG
-        (6,   5) => { 
-            for i in 0..(regx + 1) { g_register[i as usize] = memory[(*ir + i as u16) as usize] }
-            *ir = *ir + 1 + regx as u16;
-        }
+//JUMPING AND SUBROUTINES
 
-            (_,_) => ()
-    }
-}
-
-fn set_register_random (g_register: &mut [u8; 16], regx: u8, b: u8, c: u8) {
-    let rnum: u8 = random::<u8>();
-    g_register[regx as usize] = rnum & ((b << 4) + c);
+fn jump_to_address (pc: &mut usize, a: u8, b: u8, c: u8) {
+    let jump_location: u16 = (a as u16) << 8 | (b as u16) << 4 | c as u16; 
+    *pc =  jump_location as usize;
 }
 
 fn jump_v0_offset (pc: &mut usize, g_register: &[u8], a: u8, b: u8, c: u8) {
@@ -129,22 +104,10 @@ fn jump_v0_offset (pc: &mut usize, g_register: &[u8], a: u8, b: u8, c: u8) {
     *pc =  jump_location as usize;
 }
 
-fn skip_instruction_eq_register (pc: &mut usize, g_register: &[u8], regx: u8, regy: u8) {
-    if g_register[regx as usize] == g_register[regy as usize] { *pc += 2} 
-}
-
-fn skip_instruction_ne_literal (pc: &mut usize, g_register: &[u8], regx: u8, b: u8, c: u8) {
-    if g_register[regx as usize] != ((b << 4) + c) { *pc += 2} 
-}
-
-fn skip_instruction_eq_literal (pc: &mut usize, g_register: &[u8], regx: u8, b: u8, c: u8) {
-    if g_register[regx as usize] == ((b << 4) + c) { *pc += 2} 
-}
-
 fn enter_into_subroutine (pc: &mut usize, stack: &mut Vec<u16>, a: u8, b:u8, c: u8) {
     stack.push(*pc as u16);
-    jump_to_address(pc, a, b, c);
-
+    let jump_location: u16 = (a as u16) << 8 | (b as u16) << 4 | c as u16; 
+    *pc =  jump_location as usize;
 }
 
 fn return_from_subroutine (pc: &mut usize, stack: &mut Vec<u16>) {
@@ -152,9 +115,54 @@ fn return_from_subroutine (pc: &mut usize, stack: &mut Vec<u16>) {
     *pc = offset as usize;
 }
 
+
+
+
+//BASIC REGISTER OPERATIONS
+
+fn set_index_register(ir: &mut u16, a: u8, b: u8, c: u8) {
+    let reg_value: u16 = ((a as u16) << 8) | ((b as u16) << 4) | c as u16; 
+    *ir = reg_value;
+}
+
+fn set_register(g_register: &mut [u8], x: u8, b: u8, c: u8) {
+    g_register[x as usize] = (b << 4) | c;
+}
+
+fn set_register_random (g_register: &mut [u8; 16], regx: u8, b: u8, c: u8) {
+    let rnum: u8 = random::<u8>();
+    g_register[regx as usize] = rnum & ((b << 4) + c);
+}
+
+fn add_register(g_register: &mut [u8], x: u8, b: u8, c: u8) {
+    let sum: u16 = g_register[x as usize] as u16 + (b << 4 | c) as u16;
+    g_register[x as usize] = sum as u8;
+}
+
+
+
+
+
+//CONDITIONAL INSTRUCTION SKIPPING
+//3XNN
+fn skip_instruction_eq_literal (pc: &mut usize, g_register: &[u8], regx: u8, b: u8, c: u8) {
+    if g_register[regx as usize] == ((b << 4) + c) { *pc += 2} 
+}
+
+fn skip_instruction_ne_literal (pc: &mut usize, g_register: &[u8], regx: u8, b: u8, c: u8) {
+    if g_register[regx as usize] != ((b << 4) + c) { *pc += 2} 
+}
+
+fn skip_instruction_eq_register (pc: &mut usize, g_register: &[u8], regx: u8, regy: u8) {
+    if g_register[regx as usize] == g_register[regy as usize] { *pc += 2} 
+}
+
 fn skip_instruction_ne_register (pc: &mut usize, g_register: &[u8], regx: u8, regy: u8) {
     if g_register[regx as usize] != g_register[regy as usize] { *pc += 2} 
 }
+
+
+//OPERATIONS ON REGISTER VALUES
 
 fn register_operation (g_register: &mut [u8], regx: u8, regy: u8, op: u8) {
     let x = regx as usize;
@@ -189,7 +197,7 @@ fn register_operation (g_register: &mut [u8], regx: u8, regy: u8, op: u8) {
         }
         //SUBN
         0x7 => {
-            let result: u16 = g_register[y] as u16 - g_register[x] as u16;
+            let result: i16 = g_register[y] as i16 - g_register[x] as i16;
             g_register[x] = result as u8;
             if g_register[x] > g_register[y] { g_register[15] = 1 } else { g_register[15] = 0 }
             
@@ -200,17 +208,58 @@ fn register_operation (g_register: &mut [u8], regx: u8, regy: u8, op: u8) {
             g_register[x] = g_register[y] << 1;
         }
 
-        _ => {}
+        _ => ()
+    }
+}
+
+
+//SPECIAL REGISTER OPERATIONS
+fn special_register_operation (g_register: &mut [u8], memory: &mut [u8], timer_delay: &mut u8, timer_sound: &mut u8, ir: &mut u16, regx: u8, b: u8, c: u8) {
+    let x = regx as usize;
+    match (b, c) {
+
+        //TIMER INTO REG
+        (0,   7) => g_register[x] = *timer_delay,
+        //TODO: KEYPRESS TO REG
+        (0, 0xA) => (),
+        //REG INTO DELAY
+        (1,   5) => *timer_delay = g_register[x],
+        //REG INTO SOUND
+        (1,   8) => *timer_sound = g_register[x],
+        //IR = IR + REG
+        (1, 0xE) => *ir = *ir + g_register[x] as u16,
+        //TODO: IMPLEMENT LOAD SPRITE ADDR TO IR
+        (2,   9) => {
+            let location = FONT_OFFSET + (g_register[x] * 20) as usize;
+            *ir = location as u16;
+        },
+        //BINARY CODED DECIMAL
+        (3,   3) => {
+            let digits = [g_register[x] / 100, g_register[x] /10 % 10, g_register[x] % 10];
+            for i in 0..3 { memory[*ir as usize + i as usize] = digits[i] }
+        }
+        //FILL REG INTO MEM
+        (5,   5) => {
+            for i in 0..(regx + 1) { memory[(*ir + i as u16) as usize] = g_register[i as usize] }
+            *ir = *ir + 1 + regx as u16;   
+        },
+        //FILL MEM INTO REG
+        (6,   5) => { 
+            for i in 0..(regx + 1) { g_register[i as usize] = memory[(*ir + i as u16) as usize] }
+            *ir = *ir + 1 + regx as u16;
+        }
+
+        (_,_) => ()
     }
 }
 
 fn draw_sprite (display: &mut [[u8; DISPLAY_WIDTH]; DISPLAY_HEIGHT], ir: u16, g_register: &mut [u8], memory: &mut [u8; MEMORY_SIZE],  regx: u8, regy: u8, n: u8) {
 
-    let x = g_register[regx as usize];
-    let y = g_register[regy as usize];
+    let x: u16 = g_register[regx as usize] as u16;
+    let y: u16 = g_register[regy as usize] as u16;
 
-    for row in 0..n {
-        if row + y >= DISPLAY_HEIGHT as u8 { continue } 
+    for row in 0..(n as u16) {
+        if row + y >= DISPLAY_HEIGHT as u16 { continue } 
 
         let mut sprite_row_pixels: [u8; 8] = [0; 8];
         let sprite_row_byte = memory[(ir + row as u16) as usize];
@@ -221,7 +270,7 @@ fn draw_sprite (display: &mut [[u8; DISPLAY_WIDTH]; DISPLAY_HEIGHT], ir: u16, g_
 
         for col in 0..8 {
             //skip if OOB
-            if col + x >= DISPLAY_WIDTH as u8 { continue }
+            if col + x >= DISPLAY_WIDTH as u16 { continue }
             //skip if transparent
             if sprite_row_pixels[col as usize] == 0 { continue }
     
@@ -237,25 +286,9 @@ fn draw_sprite (display: &mut [[u8; DISPLAY_WIDTH]; DISPLAY_HEIGHT], ir: u16, g_
     } 
 }
 
-fn set_index_register(ir: &mut u16, a: u8, b: u8, c: u8) {
-    let reg_value: u16 = (a as u16) << 8 | (b as u16) << 4 | c as u16; 
-    *ir = reg_value;
-
-}
-
-fn add_register(g_register: &mut [u8], x: u8, b: u8, c: u8) {
-    g_register[x as usize] += b << 4 | c;
-}
-
-fn set_register(g_register: &mut [u8], x: u8, b: u8, c: u8) {
-    g_register[x as usize] = b << 4 | c;
-}
 
 
-fn jump_to_address (pc: &mut usize, a: u8, b: u8, c: u8) {
-    let jump_location: u16 = (a as u16) << 8 | (b as u16) << 4 | c as u16; 
-    *pc =  jump_location as usize;
-}
+//DISPLAY FUNCTIONS
 
 fn clear_screen (display: &mut [[u8; DISPLAY_WIDTH]; DISPLAY_HEIGHT]) {
     for i in 0..DISPLAY_HEIGHT {
@@ -270,7 +303,7 @@ fn print_display(display: &[[u8; DISPLAY_WIDTH]; DISPLAY_HEIGHT]) {
     for row in 0..DISPLAY_HEIGHT {
         for i in 0..DISPLAY_WIDTH {
             if display[row][i] == 1 { printout.push('@')}
-            else                    { printout.push('.')}
+            else                    { printout.push(' ')}
         }
         printout.push('\n');
     }
@@ -278,6 +311,11 @@ fn print_display(display: &[[u8; DISPLAY_WIDTH]; DISPLAY_HEIGHT]) {
 } 
 
 
+
+
+
+//LOADING FILES INTO SIMULATED MEMORY
+//
 fn load_font_into_memory (mem: &mut [u8]) {
     
     let mut index = FONT_OFFSET;
@@ -304,7 +342,7 @@ fn read_program_into_memory(mem: &mut [u8]) {
     let mut index = PROGRAM_OFFSET;
     const BUF_SIZE: usize = 64;
 
-    let mut f = File::open("octojam1title.ch8").unwrap();
+    let mut f = File::open("octojam6title.ch8").unwrap();
     let mut buffer: [u8; BUF_SIZE] = [0; BUF_SIZE];
 
     while index < MEMORY_SIZE {
